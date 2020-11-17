1c56443183c1da7073a333af8040e75aa44c65be
DamienBouvet22 <damien.bouvet@le-campus-numerique.fr>
2020-11-17 17:23:18 +0100

    migrations

--- migrations/Version20201117162215.php
+++ migrations/Version20201117162215.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201117131857 extends AbstractMigration
+final class Version20201117162215 extends AbstractMigration
 {
     public function getDescription() : string
     {
